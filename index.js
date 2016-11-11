0b4213290ac85ae80ebb5ccd4ac2b7d3751bf098
kurehajime <x.iidec+gh@googlemail.com>
2016-11-11 19:40:48 +0900

    mob test

--- index.js
+++ index.js
@@ -89,16 +89,13 @@
        }else{
             document.getElementById("canv").addEventListener("mousedown",newGame,false)
         }
-
-        if('devicemotion' in window){
-            window.addEventListener("devicemotion",function(e){
-                var rota_r = e.rotationRate;
-                var a = (rota_r.alpha); //z方向
-                var b = (rota_r.beta); //x方向
-                var g = (rota_r.gamma); // y方向
-                console.log(e);
-            })
-        }
+        window.addEventListener("devicemotion",function(e){
+            var rota_r = e.rotationRate;
+            var a = (rota_r.alpha); //z方向
+            var b = (rota_r.beta); //x方向
+            var g = (rota_r.gamma); // y方向
+            console.log(e);
+        })
 
 
         //画像読み込み成功時
